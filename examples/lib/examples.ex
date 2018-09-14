ac957dac3292dd5f7c7741594068d2cd458b4656
pragdave <dave@pragdave.me>
2018-09-13 19:50:46 -0500

    update examples

--- examples/lib/examples.ex
+++ examples/lib/examples.ex
@@ -1,18 +1,3 @@
 defmodule Examples do
-  @moduledoc """
-  Documentation for Examples.
-  """
 
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> Examples.hello
-      :world
-
-  """
-  def hello do
-    :world
-  end
 end
